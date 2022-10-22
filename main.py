ed2388a4837fbda104bcfc408ba14cf13b240bba
cwicksurf <chadsurf@ufl.edu>
2022-10-22 15:08:03 -0400

    "First change"

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is for lab 6 Software engineering. Pulling from Github and then pushing it back after making changes
+
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    print(add(2, 5))
